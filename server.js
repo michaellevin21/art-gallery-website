14a93a8a1d53ca74d0ba7c403e0bbef6b18522e3
michaellevin21 <112990274+michaellevin21@users.noreply.github.com>
2023-12-10 16:16:53 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 const parser = require('body-parser');
+const session = require('express-session');
 
 
 app.set("view engine", "pug");
 app.use(parser.urlencoded({extended:true}));
+app.use(session({
+    secret: 'Zt@GYMb7Nu',
+    resave: false,
+    saveUninitialized: true,
+  }));
+
+
+  function checkIfLoggedIn(req, res, next) {
+    if (req.session && req.session.userId) {
+      return res.status(403).send('<p>Another user is already logged in in this browser window</p>');
+    }
+    next();
+  }
 
 /*let userRouter = require("./user-router");
 app.use("/users", userRouter);
@@ -14,7 +28,7 @@ app.use("/products", productsRouter);
 let reviewsRouter = require("./reviews-router");
 app.use("/reviews", reviewsRouter);*/
 
-mongoose.connect('mongodb://127.0.0.1/store');
+mongoose.connect('mongodb://127.0.0.1/gallery');
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -23,7 +37,7 @@ db.once('open', async function() {
 	console.log("Server listening on port 3000");
 });
 
-let accounts = db.collections('accounts');
+let accounts = db.collection('accounts');
 
 app.get('/', function(req, res) {
 	res.render('index');
@@ -33,16 +47,15 @@ app.get('/create',function(req,res){
     res.render('create');
 });
 
-app.post('/accounts',async function(req,res){
-    if(!req.body.username || !req.body.password){
-        res.status(400);
-    }
-    else if( await accounts.findOne({username: req.body.username})){
-        res.status(409);
+app.post('/accounts',checkIfLoggedIn,async function(req,res){
+    if( await accounts.findOne({username: req.body.username})){
+        res.status(409).send('<p>This username is already taken</p>');
     }
     else{
-        await accounts.insertOne({username: req.body.username, password: req.body.password});
+        let user = {username: req.body.username, password: req.body.password, type: 'patron'};
+        await accounts.insertOne(user);
         res.status(201);
+        res.render('home',{user});
     }
 
 });
